1ea520da4feda26788fbb272453fd8fa96a26c27
webdevtoday <coderwd@gmail.com>
2025-04-14 00:37:06 +0300

    Add Menu and Booking models

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from . import models
+
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ("name", "no_of_guests", "booking_date")
+
+
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ("title", "price", "inventory")
+
 
 # Register your models here.
+admin.site.register(models.Menu, MenuAdmin)
+admin.site.register(models.Booking, BookingAdmin)
