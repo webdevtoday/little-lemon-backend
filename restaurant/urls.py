3b061fcc5e6707db18d1b9e7dce95160aaa6858c
webdevtoday <coderwd@gmail.com>
2025-04-13 23:54:32 +0300

    Add html view and static files

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from .views import sayHello
+from . import views
 
-urlpatterns = [path("", sayHello, name="sayHello")]
+urlpatterns = [
+    # path("", views.sayHello, name="sayHello"),
+    path("", views.index, name="index"),
+]
--- restaurant/views.py
+++ restaurant/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 def sayHello(request):
     return HttpResponse("Hello World")
+
+
+def index(request):
+    return render(request, "index.html", {})
